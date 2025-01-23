298bb5ea4dd0f06a9a10b3cedfedfe1b0161b5ce
Mario Rodriguez <mariorodriguez@digitalliontechnology.com>
2025-01-23 12:19:10 -0800

    state_capitals game update

--- python_student_files/state_capitals.py
+++ python_student_files/state_capitals.py
@@ -1,30 +1,141 @@
-# lab 3 project
+# Code Practice lab 3 - Simple Quiz
+# DEV 108
+# US State Capitals Game
+# Mario Rodriguez
+# January 22, 2025
+from python.book_apps.ch04.guess_the_number import play_game
 
-import random
-
+# import random
+#===========================================================================
 # TO DO
 # Draft welcome message for the user with rules for the game
 # Generate five random numbers from 1-50 to pick five random US States
+# User response validation
+#===========================================================================
 
+# Dictionary of US States and US State Capitals
+usStateCapitals = { #fill out entire dictionary with all US states
+    "Alabama":"Montgomery",
+    "Alaska":"Juneau",
+    "Arizona":"Phoenix",
+    "Arkansas":"Little Rock",
+    "California":"Sacramento",
+    "Colorado":"Denver",
+    "Connecticut": "hartford",
+    "Delaware":"Dover",
+    "Florida":"Tallahassee",
+    "Georgia":"Atlanta",
+    "Hawaii":"Honolulu",
+    "Idaho":"Boise",
+    "Illinois":"Springfield",
+    "Indiana":"Indianapolis",
+    "Iowa":"Des Moines",
+    "Kansas":"Topeka",
+    "Kentucky":"Frankfort",
+    "Louisiana":"Baton Rouge",
+    "Maine":"Augusta",
+    "Maryland":"Annapolis",
+    "Massachusetts":"Boston",
+    "Michigan":"Lansing",
+    "Minnesota":"Saint Paul",
+    "Mississippi":"Jackson",
+    "Missouri":"Jefferson City",
+    "Montana":"Helena",
+    "Nebraska":"Lincoln",
+    "Nevada":"Carson City",
+    "New Hampshire":"Concord",
+    "New Jersey":"Trenton",
+    "New Mexico":"Santa Fe",
+    "New York":"Albany",
+    "North Carolina":"Raleigh",
+    "North Dakota":"Bismarck",
+    "Ohio":"Columbus",
+    "Oklahoma":"Oklahoma City",
+    "Oregon":"Salem",
+    "Pennsylvania":"Harrisburg",
+    "Rhode Island":"Providence",
+    "South Carolina":"Columbia",
+    "South Dakota":"Pierre",
+    "Tennessee":"Nashville",
+    "Texas":"Austin",
+    "Utah":"Salt Lake City",
+    "Vermont":"Montpelier",
+    "Virginia":"Richmond",
+    "Washington":"Olympia",
+    "West Virginia":"Charleston",
+    "Wisconsin":"Madison",
+    "Wyoming":"Cheyenne"
+}
 
-usStateCapitals = {
-    "washington":"olympia", } #fill out entire dictionary with all US states
-
-usStates = ["washington", "oregon", "california", "nevada", "arizona"] # test list to be replaced with random generated states
-
-userResponses = [] # collect user responses
+usStates = ["Washington", "Oregon", "California", "Nevada", "Arizona"] # test list to be replaced with random generated states
+stateCapital = ["Olympia", "Salem", "Sacramento", "Carson City", "Phoenix"] #test list to be replaced with random generated states
 
 #===== Welcome Message =====
-print("Welcome to the game")
-print(usStateCapitals.get(usStates[0]))
+# print("Welcome to the game")
+# print(usStateCapitals.get(usStates[0])) #use for test
 
-# getUserResponse = int(input("Enter a number: "))
+def welcome_message():
+    print(f'==========================================')
+    print(f'         US State Capitals Game           ')
+    print(f'==========================================')
+    print(f'Would you like to test your knowledge of \nUS State Capitals?')
+    play_game = (input(f'Please type "yes" or no to continue: '))
 
-for i in range(len(usStates)):
-    print(f"What is the capital of {usStates[i]}?")
-    userResponses.insert(i, input("Enter your response: "))
+    return play_game
 
-for i in userResponses:
-    print(i)
+def goodbye_message():
+    print(f'See you next time. Goodbye! ')
+
+def the_game():
+
+    user_response = []  # collect user responses
+    user_score = 0
+
+    for i in range(len(usStates)):
+        print(f"What is the capital of {usStates[i]}?\n")
+        print(f"1. {stateCapital[0]}\n"
+              f"2. {stateCapital[1]}\n"
+              f"3. {stateCapital[2]}\n"
+              f"4. {stateCapital[3]}\n"
+              f"5. {stateCapital[4]}")
+        print()
+        user_response.insert(i, input("Enter your guess: "))
+        print()
+        if stateCapital[int(user_response[i])-1] == usStateCapitals.get(usStates[i]):
+            user_score += 1
+            print(f'That is correct!')
+            print()
+        else:
+            print(f'Sorry, {stateCapital[int(user_response[i])]} is not the capital of {usStates[i]}')
+            print()
+    # Print user score
+    if user_score == 5:
+        print(f'Your Total Score: {user_score}. You are a Rockstar!')
+        print()
+    elif user_score == 4:
+        print(f'Your Total Score: {user_score}. Almost 100%!')
+        print()
+    elif user_score == 3 or user_score == 2:
+        print(f'Your Total Score: {user_score}. Keep studying!')
+        print()
+    elif user_score <= 2:
+        print(f'Your Total Score: {user_score}. Maybe this is not your area of interest!')
+        print()
+
+    # for i in user_response:  # print out user responses as test
+        # print(i)
+
+def main():
+    start_game = welcome_message()
+    if start_game == "yes":
+        the_game()
+    else:
+        goodbye_message()
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
