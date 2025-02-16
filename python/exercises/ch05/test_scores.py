0cf85f43a667d312e9f5df1b55d6f174a347d84d
Mario Rodriguez <mariorodriguezdeleon@gmail.com>
2025-02-15 17:14:11 -0800

    homework february 15

--- python/exercises/ch05/test_scores.py
+++ python/exercises/ch05/test_scores.py
@@ -16,15 +16,16 @@ print("======================")
 # initialize variables
 counter = 0
 score_total = 0
-test_score = 0
+# test_score = 0
 
 while True:
 
+    # request input from the user
     test_score = input("Enter test score (or 'x' to quit): ")
 
+    # test to see if the input is the exit character for the program
     if test_score.lower() != "x":
-        test_score = int(test_score)
-        # counter += 1 # remove counter
+        test_score = int(test_score) # if input is not exit character convert input to integer
     else:
         break
     if test_score >= 0 and test_score <= 100:
@@ -33,6 +34,7 @@ while True:
     else:
         print("Test score must be from 0 through 100. Score discarded. Try again.")   
 
+# added this check to ensure that if the user exit the program at initiation the division by 0 is mitigated
 if counter == 0:
     counter += 1
 
@@ -45,5 +47,3 @@ print(f"Total Score: {score_total}"
       f"\nAverage Score: {average_score}")
 print()
 print("Bye")
-
-
