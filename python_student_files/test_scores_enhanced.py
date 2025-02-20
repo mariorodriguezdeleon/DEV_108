2c7f4765c77a99681b23eed86ca59e83510068a7
Mario Rodriguez <mariorodriguez@digitalliontechnology.com>
2025-02-19 17:18:55 -0800

    test_scores_enhanced update

--- python_student_files/test_scores_enhanced.py
+++ python_student_files/test_scores_enhanced.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 #===========================================================================
-# Code Practice Lab 4 - Test Scores Enhanced
+# Code Practice Lab 7 - Test Scores Enhanced
+# Leverage a Python List data structure, and functions
 # DEV 108 - 34634
 # Mario Rodriguez
-# January 27, 2025
+# February 19, 2025
 #===========================================================================
 
 # display a welcome message
@@ -17,7 +18,7 @@ while True:
 
     total_score = 0
     count = 0
-    test_scores = []
+    
 
     # Inner loop to input scores for a single set
     while True:
@@ -62,16 +63,17 @@ while True:
         print("===============================")
 
 
-def get_scores():
+def get_scores(test_scores):
     print('get_scores')
 
-def process_scores():
+def process_scores(test_scores):
     print('process_scores')
 
 def main():
+    test_scores = []
     print('main()')
-    get_scores()
-    process_scores()
+    get_scores(test_scores)
+    process_scores(test_scores)
 
 if __name__ == '__main__':
     main()
