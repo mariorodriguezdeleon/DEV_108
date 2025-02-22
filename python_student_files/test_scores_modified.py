cbcf92b1cdd5d2ae98ee8f58992269fbd3125223
Mario Rodriguez <mariorodriguez@digitalliontechnology.com>
2025-02-22 15:31:30 -0800

    homework lab 7

--- python_student_files/test_scores_modified.py
+++ python_student_files/test_scores_modified.py
@@ -4,7 +4,7 @@
 # Leverage a Python List data structure, and functions
 # DEV 108 - 34634
 # Mario Rodriguez
-# February 19, 2025
+# February 22, 2025
 #===========================================================================
 
 # display a welcome message
@@ -13,19 +13,13 @@ print("Enter test scores")
 print("Enter 'end' to end input")
 print("===============================")
 
-# Outer loop to manage multiple sets of scores
-while True:
-
-    total_score = 0
-    count = 0
-    
-
-    # Inner loop to input scores for a single set
+def get_scores(test_scores):
     while True:
+
         score_input = input("Enter test score: ")
 
         # Check if input is "end"
-        if score_input.lower() == "end":
+        if score_input.lower() == "x":
             break
 
         # Convert input to integer for processing
@@ -37,45 +31,43 @@ while True:
         if 0 <= score <= 100:
             # ---- Your code here ----------
             test_scores.append(score)
-            total_score += score
-            count += 1
         else:
             # ---- Your code here ----------
             print(f'Test Score must be from 0 through 100. Try again.')
 
-    # Calculate the average score
-    # ---- Your code here ----------
-    average_score = int(total_score/count)
-
-    print('===============================')
-    print(f'Total Score: {total_score}')
-    print(f'Average Score: {average_score}')
-    print(test_scores)
-    print()
-
-    # Ask the user if they want to enter another set of scores
-    another_set = input("Enter another set of test scores (y/n)? ")
-    if another_set.lower() != "y":
-        break
-    else:
-        print("\nEnter test scores")
-        print("Enter 'end' to end input")
-        print("===============================")
-
-def get_scores(test_scores):
-    
-
 def process_scores(test_scores):
-    print('process_scores')
+    
+    print(test_scores)
+
+    # sum all scores
+
+    # print total number of scores (list len)
+
+    # print AVerage Score
+
+    # print low score
+
+    # print high score
+
+    # print median score (not average but score in the middle)
 
 def main():
 
-    test_scores = [get_scores()]
-    
-    
-    
-    # get_scores(test_scores)
-    # process_scores(test_scores)
+    while True:
+
+        test_scores = []
+        get_scores(test_scores)
+
+        # process_scores(test_scores)
+        print (test_scores)
+
+        another_set = input("Enter another set of test scores (y/n)? ")
+        if another_set.lower() != "y":
+            break
+        else:
+            print("\nEnter test scores")
+            print("Enter 'x' to end input")
+            print("===============================")
 
 if __name__ == '__main__':
     main()
