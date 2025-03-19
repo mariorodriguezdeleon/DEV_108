372a589012ea5ce004f4b49347f53158a4b5da79
Mario Rodriguez <mariorodriguez@digitalliontechnology.com>
2025-03-19 11:18:59 -0700

    update quiz 5

--- python_student_files/quiz_7.py
+++ python_student_files/quiz_7.py
@@ -35,17 +35,35 @@
 # if __name__ == "__main__":
 #     main() 
 
-def main():
+# def main():
 
-    furry_pets = ["dog", "cat", "ferret", "hamster", "bunny"]
-    feathered_pets = ["canary", "parrot", "budgie", "hawk"]
-    all_pets = furry_pets + feathered_pets
-    new_pets =[]
-    i = 0
-    for item in all_pets:
-        if item[i][0] == "c":
-            new_pets.append(item)
-    print("The pet store sells:", all_pets)
-    print("These start with the letter c:", new_pets)
+#     furry_pets = ["dog", "cat", "ferret", "hamster", "bunny"]
+#     feathered_pets = ["canary", "parrot", "budgie", "hawk"]
+#     all_pets = furry_pets + feathered_pets
+#     new_pets =[]
+#     i = 0
+#     for item in all_pets:
+#         if item[i][0] == "c":
+#             new_pets.append(item)
+#     print("The pet store sells:", all_pets)
+#     print("These start with the letter c:", new_pets)
 
-main()
+# main()
+
+# car = "PORSCHE"
+# color = "red"
+# my_car = car.join(color)
+# print(my_car)
+
+# book_name = "a tale for the knight"
+# book = book_name.title()
+# print(book)
+
+# name = "Mervin the Magician"
+# words = name.split()
+# print(words[0] + ", you are quite a " + words[2].lower())
+
+# firstName = "Mickey"
+# lastName = "Mouse"
+# myName = firstName + lastName
+# print(myName.lower())
